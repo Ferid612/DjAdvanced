05645ea44aef89ed1e87a5afe93d8d252f68817d
Ferid612 <hebibliferid20@gmail.com>
2023-05-22 20:17:45 +0400

    save

--- DjApp/app_urls/urls_search.py
+++ DjApp/app_urls/urls_search.py
@@ -2,5 +2,5 @@ from django.urls import path
 from DjApp.views import views_search
 
 urlpatterns = [
-    path('search/', views_search.search_product, name="search"),
+    path('', views_search.search_product, name="search"),
    ]
