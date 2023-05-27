01020e782979b1c1b6d49b7ec2739a4b3c66af9c
Ferid612 <hebibliferid20@gmail.com>
2023-05-27 15:34:09 +0400

    save

--- DjAdvanced/settings/base.py
+++ DjAdvanced/settings/base.py
@@ -2,6 +2,12 @@ import os
 from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
