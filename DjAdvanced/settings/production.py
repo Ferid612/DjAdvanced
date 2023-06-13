34361f7d8f0d28f9d2a60c307b3d30d0b694c227
Ferid612 <hebibliferid20@gmail.com>
2023-06-13 20:18:59 +0400

    test nettfly

--- DjAdvanced/settings/production.py
+++ DjAdvanced/settings/production.py
@@ -1,7 +1,7 @@
 from ..settings.base import *
 
 HOST_URL = "e-delta.store"
-ALLOWED_FRONT_HOST = "https://e-delta.netlify.app"
+ALLOWED_FRONT_HOST = "e-delta.netlify.app"
 DEBUG = True
 
 ALLOWED_HOSTS = ['e-delta.azurewebsites.net', 'www.e-delta.azurewebsites.net', 'e-delta.store', 'www.e-delta.store', 'e-delta.netlify.app', 'www.e-delta.netlify.app']
