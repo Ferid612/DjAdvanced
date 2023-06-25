010eaaf7476296a02f623a7cc867facc778c7a81
Ferid612 <hebibliferid20@gmail.com>
2023-06-25 18:42:41 +0400

    cheking

--- DjAdvanced/settings/production.py
+++ DjAdvanced/settings/production.py
@@ -4,4 +4,5 @@ HOST_URL = "e-delta.store"
 ALLOWED_FRONT_HOST = "https://e-delta.netlify.app"
 DEBUG = True
 
-ALLOWED_HOSTS = ['e-delta.azurewebsites.net', 'www.e-delta.azurewebsites.net', 'e-delta.store', 'www.e-delta.store', 'e-delta.netlify.app', 'www.e-delta.netlify.app']
+ALLOWED_HOSTS = ['e-delta.azurewebsites.net', 'www.e-delta.azurewebsites.net', 'e-delta.store', 'www.e-delta.store', 'e-delta.netlify.app', 
+                 'www.e-delta.netlify.app', 'djadvanced2-05b7cc3fc5d9.herokuapp.com' ]
