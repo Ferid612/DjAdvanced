9c96cd94ac211ee2892c34b5fa16a004b32cde70
Ferid612 <hebibliferid20@gmail.com>
2023-06-27 20:01:56 +0400

    security max

--- DjAdvanced/middleware.py
+++ DjAdvanced/middleware.py
@@ -1,11 +1,12 @@
 from django.shortcuts import redirect
+from django.utils.deprecation import MiddlewareMixin
 
-class RedirectMiddleware:
+class RedirectMiddleware(MiddlewareMixin):
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        # print(request.META['HTTP_HOST'])
-        if request.META['HTTP_HOST'] == 'e-delta.shop':
-            return redirect('https://www.e-delta.shop')
+        if not request.is_secure() and request.META.get('HTTP_X_FORWARDED_PROTO') != 'https':
+            secure_url = request.build_absolute_uri(request.get_full_path()).replace('http://', 'https://')
+            return redirect(secure_url)
         return self.get_response(request)
