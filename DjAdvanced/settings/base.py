040ea0c168ed955ff9da321fcc59e5a8907baf86
Farid612 <hebibliferid20@mail.com>
2024-11-21 14:36:40 +0400

    "Updated settings module references in asgi.py, wsgi.py, and manage.py to point to 'DjAdvanced.settings.development' instead of 'DjAdvanced.settings' or 'DjAdvanced.settings.production'. Commented out 'Dj

--- DjAdvanced/settings/base.py
+++ DjAdvanced/settings/base.py
@@ -92,7 +92,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'DjAdvanced.middleware.RedirectMiddleware',
+    # 'DjAdvanced.middleware.RedirectMiddleware',
 ]
 
 ROOT_URLCONF = 'DjAdvanced.urls'
--- DjAdvanced/settings/production.py
+++ DjAdvanced/settings/production.py
@@ -5,5 +5,5 @@ ALLOWED_FRONT_HOST = "https://e-delta.netlify.app"
 DEBUG = True
 # Test
 
-# ALLOWED_HOSTS = ['e-delta.azurewebsites.net', 'www.e-delta.azurewebsites.net', 'e-delta.store', 'www.e-delta.store', 'e-delta.netlify.app', 'www.e-delta.netlify.app', 'djadvanced2-05b7cc3fc5d9.herokuapp.com']
-ALLOWED_HOSTS =['*']
+ALLOWED_HOSTS = ['e-delta.azurewebsites.net', 'www.e-delta.azurewebsites.net', 'e-delta.store', 'www.e-delta.store', 'e-delta.netlify.app', 'www.e-delta.netlify.app', 'djadvanced2-05b7cc3fc5d9.herokuapp.com']
+# ALLOWED_HOSTS =['*']
